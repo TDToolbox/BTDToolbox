06613cf008ee73d74e0284a97e33afb868f04e3b
MallisOG <mallisiscool@gmail.com>
2019-09-09 19:24:32 -0400

    start new project format

---
+++ BTDToolbox/ProjectFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTDToolbox
+{
+    class ProjectFile
+    {
+        public string name;
+        public string executable;
+        public string assets;
+        public string steam_api;
+        public string jet;
+        public Games game;
+
+        public ProjectFile(string name, string executable, string assets, string steam_api, string jet, Games game)
+        {
+
+        }
+    }
+}
