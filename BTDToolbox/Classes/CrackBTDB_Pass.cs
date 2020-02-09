be22f7fcdad4efd1d97b597051c3cc65e2933b88
Gurrenm3 <ThomasPMar@hotmail.com>
2020-02-09 13:42:29 -0800

    finished adding download and extraction for btdb pass tools

--- BTDToolbox/Classes/CrackBTDB_Pass.cs
+++ BTDToolbox/Classes/CrackBTDB_Pass.cs
@@ -38,16 +38,18 @@ namespace BTDToolbox.Classes
         {
             CheckForDependencies();
 
-            DialogResult reuslt = MessageBox.Show("Unfortunately it will take up to 15 minutes to download the necessary tools. This is a One Time thing.\n\nDo you wish to continue?", "Do you wish to continue?", MessageBoxButtons.YesNo);
+            DialogResult reuslt = MessageBox.Show("It will take up to 5 minutes to download the necessary tools. This is a One Time thing.\n\nDo you wish to continue?", "Do you wish to continue?", MessageBoxButtons.YesNo);
             if (reuslt == DialogResult.Yes)
             {
-                
                 DownloadTools();
 
-                /*ConsoleHandler.appendLog("Extracting strings2.zip");
+                ConsoleHandler.appendLog("Extracting strings2.zip");
                 ExtractTools(Strings2Dir + "\\strings2.zip", Strings2Dir);
                 ConsoleHandler.appendLog("Extracting johntr.zip");
-                ExtractTools(JohntrDir + "\\johntr.zip", JohntrDir);*/
+                ExtractTools(JohntrDir + "\\johntr.zip", JohntrDir);
+
+                File.Delete(Strings2Dir + "\\strings2.zip");
+                File.Delete(JohntrDir + "\\johntr.zip");
             }
             else
             {
@@ -80,51 +82,51 @@ namespace BTDToolbox.Classes
             string DirPropmts_url = "https://www.dropbox.com/s/9xumg3cvo3gbo5x/Directory_Prompts.reg?dl=0";
 
             
-                if (GetOS_Type() == "64")    //Get 64bit version of tools, otherwise get 32bit version
-                {
-                    //strings2_url = "http://split-code.com/files/strings2_x64_v1-2.zip";
-                    strings2_url = "https://www.dropbox.com/s/6hiifxlffqrizeu/john-1.9.0-jumbo-1-win64.zip?dl=0";
-                    johntr_url = "https://www.openwall.com/john/k/john-1.9.0-jumbo-1-win64.zip";
-                }
-                else
-                {
-                    strings2_url = "http://split-code.com/files/strings2_x86_v1-2.zip";
-                    johntr_url = "https://www.openwall.com/john/k/john-1.9.0-jumbo-1-win32.zip";
-                }
+            if (GetOS_Type() == "64")    //Get 64bit version of tools, otherwise get 32bit version
+            {
+                strings2_url = "http://split-code.com/files/strings2_x64_v1-2.zip";
+                johntr_url = "https://www.dropbox.com/s/6hiifxlffqrizeu/john-1.9.0-jumbo-1-win64.zip?dl=1";
+            }
+            else
+            {
+                strings2_url = "http://split-code.com/files/strings2_x86_v1-2.zip";
+                johntr_url = "https://www.dropbox.com/s/t3pd2ypnhqo9n0f/john-1.9.0-jumbo-1-win32.zip?dl=1";
+            }
 
-                //Create directories if they're not there
-                if (!Directory.Exists(DirPrompts_Dir))
-                    Directory.CreateDirectory(DirPrompts_Dir);
-                if (!Directory.Exists(Strings2Dir))
-                    Directory.CreateDirectory(Strings2Dir);
-                if (!Directory.Exists(JohntrDir))
-                    Directory.CreateDirectory(JohntrDir);
+            //Create directories if they're not there
+            if (!Directory.Exists(DirPrompts_Dir))
+                Directory.CreateDirectory(DirPrompts_Dir);
+            if (!Directory.Exists(Strings2Dir))
+                Directory.CreateDirectory(Strings2Dir);
+            if (!Directory.Exists(JohntrDir))
+                Directory.CreateDirectory(JohntrDir);
                 
 
-                //Delete previous files if they ARE there
-                if (File.Exists(DirPrompts_Dir + "\\Directory_Prompts.reg"))
-                    File.Delete(DirPrompts_Dir + "\\Directory_Prompts.reg");
+            //Delete previous files if they ARE there
+            if (File.Exists(DirPrompts_Dir + "\\Directory_Prompts.reg"))
+                File.Delete(DirPrompts_Dir + "\\Directory_Prompts.reg");
 
-                if (File.Exists(Strings2Dir + "\\strings2.zip"))
-                    File.Delete(Strings2Dir + "\\strings2.zip");
+            if (File.Exists(Strings2Dir + "\\strings2.zip"))
+                File.Delete(Strings2Dir + "\\strings2.zip");
 
-                if (File.Exists(JohntrDir + "\\johntr.zip"))
-                    File.Delete(JohntrDir + "\\johntr.zip");
+            if (File.Exists(JohntrDir + "\\johntr.zip"))
+                File.Delete(JohntrDir + "\\johntr.zip");
 
 
-                //Download files
-                ConsoleHandler.appendLog("Downloading Directory Prompts...");
-                Client.DownloadFile(DirPropmts_url, DirPrompts_Dir + "\\Directory_Prompts.reg");
-                ConsoleHandler.appendLog("Finished downloading Directory Prompts.");
+            //Download files
+            ConsoleHandler.appendLog("Downloading Directory Prompts...");
+            Client.DownloadFile(DirPropmts_url, DirPrompts_Dir + "\\Directory_Prompts.reg");
+            ConsoleHandler.appendLog("Finished downloading Directory Prompts.");
 
-                ConsoleHandler.appendLog("Downloading strings2...");
-            Client.DownloadFile(strings2_url, Strings2Dir + "\\strings2.zip");
-                ConsoleHandler.appendLog("Finished downloading strings2.");
+            ConsoleHandler.appendLog("Downloading strings2...");
+            Client.DownloadFile(strings2_url, Strings2Dir + "\\strings2");
+            File.Move(Strings2Dir + "\\strings2", Strings2Dir + "\\strings2.zip");
+            ConsoleHandler.appendLog("Finished downloading strings2.");
 
-                ConsoleHandler.appendLog("Downloading johntr. Unfortunately this will take up to 15 minutes...\n>> If the program freezes, just leave it.");
-                //Client.DownloadFile(johntr_url, JohntrDir + "\\johntr.zip");
-                ConsoleHandler.appendLog("Finished downloading johntr");
-                ConsoleHandler.appendLog("Thank you for being patient. The tools have finished downloading.");
+            ConsoleHandler.appendLog("Downloading johntr. This will take up to 5 minutes...\n>> If the program freezes, just leave it.");
+            Client.DownloadFile(johntr_url, JohntrDir + "\\johntr.zip");
+            ConsoleHandler.appendLog("Finished downloading johntr");
+            ConsoleHandler.appendLog("Thank you for being patient. The tools have finished downloading.");
         }
         private void ExtractTools(string zipPath, string outputPath)
         {
