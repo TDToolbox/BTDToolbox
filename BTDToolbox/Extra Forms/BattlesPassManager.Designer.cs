20e1b14b46c85486c37fb6bfe1d841fd3561d98f
Gurrenm3 <ThomasPMar@hotmail.com>
2020-03-28 18:35:04 -0700

    Battles pass manager ui done

--- BTDToolbox/Extra Forms/BattlesPassManager.Designer.cs
+++ BTDToolbox/Extra Forms/BattlesPassManager.Designer.cs
@@ -29,17 +29,19 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.Pass_LB = new System.Windows.Forms.ListBox();
+            this.Game_LB = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Versions_CB = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Password_TB = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.Copy_Button = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.VersionNum_TB = new System.Windows.Forms.RichTextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,24 +56,24 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Battles Password Maganger";
             // 
-            // Pass_LB
+            // Game_LB
             // 
-            this.Pass_LB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
-            this.Pass_LB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Pass_LB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Pass_LB.ForeColor = System.Drawing.Color.White;
-            this.Pass_LB.FormattingEnabled = true;
-            this.Pass_LB.ItemHeight = 25;
-            this.Pass_LB.Items.AddRange(new object[] {
+            this.Game_LB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.Game_LB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Game_LB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Game_LB.ForeColor = System.Drawing.Color.White;
+            this.Game_LB.FormattingEnabled = true;
+            this.Game_LB.ItemHeight = 25;
+            this.Game_LB.Items.AddRange(new object[] {
             "Steam",
             "Windows",
             "Android",
             "IOS",
             "Mac"});
-            this.Pass_LB.Location = new System.Drawing.Point(185, 124);
-            this.Pass_LB.Name = "Pass_LB";
-            this.Pass_LB.Size = new System.Drawing.Size(324, 175);
-            this.Pass_LB.TabIndex = 1;
+            this.Game_LB.Location = new System.Drawing.Point(185, 124);
+            this.Game_LB.Name = "Game_LB";
+            this.Game_LB.Size = new System.Drawing.Size(543, 175);
+            this.Game_LB.TabIndex = 1;
             // 
             // panel1
             // 
@@ -98,15 +100,15 @@
             this.button2.Text = "Passwords";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // comboBox1
+            // Versions_CB
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.ItemHeight = 20;
-            this.comboBox1.Location = new System.Drawing.Point(185, 75);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(324, 28);
-            this.comboBox1.TabIndex = 3;
+            this.Versions_CB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Versions_CB.FormattingEnabled = true;
+            this.Versions_CB.ItemHeight = 20;
+            this.Versions_CB.Location = new System.Drawing.Point(185, 75);
+            this.Versions_CB.Name = "Versions_CB";
+            this.Versions_CB.Size = new System.Drawing.Size(324, 28);
+            this.Versions_CB.TabIndex = 3;
             // 
             // label2
             // 
@@ -135,15 +137,15 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Visible = false;
             // 
-            // richTextBox1
+            // Password_TB
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(185, 339);
-            this.richTextBox1.Multiline = false;
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(324, 35);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
+            this.Password_TB.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_TB.Location = new System.Drawing.Point(185, 339);
+            this.Password_TB.Multiline = false;
+            this.Password_TB.Name = "Password_TB";
+            this.Password_TB.Size = new System.Drawing.Size(324, 35);
+            this.Password_TB.TabIndex = 5;
+            this.Password_TB.Text = "";
             // 
             // label3
             // 
@@ -156,35 +158,35 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Password:";
             // 
-            // button3
+            // Save_Button
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(614, 339);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(80, 35);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Save";
-            this.button3.UseVisualStyleBackColor = false;
+            this.Save_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.Save_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Save_Button.FlatAppearance.BorderSize = 0;
+            this.Save_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save_Button.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save_Button.ForeColor = System.Drawing.Color.White;
+            this.Save_Button.Location = new System.Drawing.Point(614, 339);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(80, 35);
+            this.Save_Button.TabIndex = 7;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // Copy_Button
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(528, 339);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(80, 35);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Copy";
-            this.button4.UseVisualStyleBackColor = false;
+            this.Copy_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.Copy_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Copy_Button.FlatAppearance.BorderSize = 0;
+            this.Copy_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Copy_Button.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Copy_Button.ForeColor = System.Drawing.Color.White;
+            this.Copy_Button.Location = new System.Drawing.Point(528, 339);
+            this.Copy_Button.Name = "Copy_Button";
+            this.Copy_Button.Size = new System.Drawing.Size(80, 35);
+            this.Copy_Button.TabIndex = 8;
+            this.Copy_Button.Text = "Copy";
+            this.Copy_Button.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
@@ -201,22 +203,47 @@
             this.button5.Text = "+";
             this.button5.UseVisualStyleBackColor = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(596, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Version number";
+            this.label4.Visible = false;
+            // 
+            // VersionNum_TB
+            // 
+            this.VersionNum_TB.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionNum_TB.Location = new System.Drawing.Point(575, 74);
+            this.VersionNum_TB.Multiline = false;
+            this.VersionNum_TB.Name = "VersionNum_TB";
+            this.VersionNum_TB.Size = new System.Drawing.Size(153, 35);
+            this.VersionNum_TB.TabIndex = 10;
+            this.VersionNum_TB.Text = "";
+            this.VersionNum_TB.Visible = false;
+            // 
             // BattlesPassManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(756, 406);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.VersionNum_TB);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Copy_Button);
+            this.Controls.Add(this.Save_Button);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Password_TB);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Versions_CB);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Pass_LB);
+            this.Controls.Add(this.Game_LB);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "BattlesPassManager";
@@ -231,16 +258,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox Pass_LB;
+        private System.Windows.Forms.ListBox Game_LB;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Versions_CB;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox Password_TB;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button Copy_Button;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox VersionNum_TB;
     }
 }
